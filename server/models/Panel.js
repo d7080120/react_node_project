2e5cb277e0e62fd20bddb4b8e56174c5db061848
d7080120 <38327856894@mby.co.il>
2025-01-05 15:36:12 +0200

    add panel model

--- server/models/Panel.js
+++ server/models/Panel.js
@@ -6,9 +6,33 @@ const panelSchema = new mongoose.Schema({
     },
     questions:{
         type:[{
-         
+         title:{type:String,required},
+         questionType:{enum:["text","check1","checkMore","range"]},
+         cotents:{type:String,required},
+         answers:[string],
+         userAnswers:[
+            {
+                answer:int,
+                body:string,
+                participant:{type: mongoose.ObjectId, ref: 'Participant'}
+        
+
+            }
+         ],
+         cost:{
+            type:mongoose.Schema.Types.Double
+        },
+        score:{
+            type:int
+        },
+        analysis_question:{
+            type: mongoose.ObjectId, ref: 'Analysis_question', 
+            
+        },
+        extra:[{enum:["must","withOpenText","image"]}],
         }
         ]
+
     },
     cost:{
         type:mongoose.Schema.Types.Double
@@ -16,14 +40,19 @@ const panelSchema = new mongoose.Schema({
     score:{
         type:int
     },
-    numsOfParticipents:{
+    numsOfParticipants:{
         type:int,
         default:1000
     },
-    analysis:{
-        type: mongoose.ObjectId, ref: 'Analysis', 
+    analysis_panel:{
+        type: mongoose.ObjectId, ref: 'Analysis_panel', 
         
     },
+    Constraints:{type:[{
+        enum:["age","gender","location"],
+        body:{type:String,required},
+    }]
+    }
 }, {
     timestamps: true
 })
